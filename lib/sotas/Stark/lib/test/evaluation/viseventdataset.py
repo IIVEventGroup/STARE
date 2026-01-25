7a4891b80b5d4a45c846f1bc549207bb783a52c9
Runze Zhang <zhangrunze0311@gmail.com>
2026-01-25 22:57:18 +0800

    adjust details for final revision

--- lib/sotas/Stark/lib/test/evaluation/viseventdataset.py
+++ lib/sotas/Stark/lib/test/evaluation/viseventdataset.py
@@ -16,6 +16,7 @@ class VisEventDataset(BaseDataset):
 
     Download dataset from http://got-10k.aitestunion.com/downloads
     """
+
     def __init__(self, split, version=None):
         super().__init__()
         # Split can be test, val, or ltrval (a validation split consisting of videos from the official train set)
@@ -24,7 +25,7 @@ class VisEventDataset(BaseDataset):
         else:
             self.base_path = os.path.join(self.env_settings.visEvent_dir, 'train')
 
-        self.sequence_list,_ = self._get_sequence_list_start(split,version)
+        self.sequence_list, _ = self._get_sequence_list_start(split, version)
         self.split = split
 
     def get_sequence_list(self):
@@ -35,7 +36,7 @@ class VisEventDataset(BaseDataset):
 
         ground_truth_rect = load_text(str(anno_path), delimiter=',', dtype=np.float64)
 
-        frames_path = '{}/{}/{}'.format(self.base_path, sequence_name,'VoxelGridComplex')
+        frames_path = '{}/{}/{}'.format(self.base_path, sequence_name, 'VoxelGridComplex')
         frame_list = sorted(os.listdir(frames_path))
         # frame_list.sort(key=lambda f: int(f[:-4]))
         frames_list = [os.path.join(frames_path, frame) for frame in frame_list]
@@ -48,9 +49,9 @@ class VisEventDataset(BaseDataset):
     def _get_sequence_list(self, split):
         sequence_list = []
         list_file = '{}/{}.txt'.format(self.base_path, split)
-        with open (list_file,'r') as f:
+        with open(list_file, 'r') as f:
             for line in f:
-                sequence_list.append(split+'/'+line.strip()) 
+                sequence_list.append(split + '/' + line.strip())
 
         return sequence_list
 
@@ -59,29 +60,35 @@ class VisEventDataset(BaseDataset):
         seq2start = []
         version_list = []
         list_pair_file = '{}/{}_pair.json'.format(self.env_settings.visEvent_dir, split)
-        with open(list_pair_file,'r') as j:
+        with open(list_pair_file, 'r') as j:
             seq_start_dict = json.loads(j.read())
+
         # build version list
-        if version in ['stnet','presence','172']:
+        if version in ['stnet', 'presence', '172']:
             version_list_file = '{}/{}_{}.txt'.format(self.env_settings.visEvent_dir, split, version)
-            with open(version_list_file,'r') as v:
+            with open(version_list_file, 'r') as v:
                 for line in v.readlines():
                     version_list.append(line.strip())
         version_list_cp = version_list.copy()
-        for seq,start_idx in seq_start_dict.items():
-            if len(version_list)==0: # no version
+
+        for seq, start_idx in seq_start_dict.items():
+            if len(version_list) == 0:  # no version
                 sequence_list.append(seq)
                 seq2start.append(start_idx)
-            else: # version selected
+            else:  # version selected
                 if seq in version_list:
                     sequence_list.append(seq)
                     seq2start.append(start_idx)
                     version_list_cp.remove(seq)
-        if len(version_list_cp)>0:
+
+        if len(version_list_cp) > 0:
             print('Missing sequence')
             print(version_list_cp)
+
         return sequence_list, seq2start
+
+
 if __name__ == '__main__':
-    train_dataset = VisEventDataset('test','stnet')
+    train_dataset = VisEventDataset('test', 'stnet')
     seqlist = train_dataset.get_sequence_list()
     print(seqlist)
